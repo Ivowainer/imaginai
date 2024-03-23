20cb98bb716a8f85ef6a44b66ee8bdb0dfb6e638
Ivowainer <ivancamposwainer@gmail.com>
2024-03-23 00:08:01 -0300

    Clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-    return <div className="">Home</div>;
+    return (
+        <div className="">
+            <p>Home</p>
+            <UserButton afterSignOutUrl='/' />
+        </div>
+    );
 }
